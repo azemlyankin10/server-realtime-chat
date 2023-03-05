49b114e92d08df1a9ab71b9979add7991d6082dd
Andrey Zemlyankin <azemlyankin10@gmail.com>
2023-03-05 01:11:00 +0100

    change url cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ const initializeSocket = require('./socket/socket');
 const app = express();
 // Allow cross-origin requests from http://localhost:4200
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        'https://angular-nodejs-realtime-frontend.vercel.app'
+    );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
