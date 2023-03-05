abec04b81cafd34495947a7584966dcf5a788d38
Andrey Zemlyankin <azemlyankin10@gmail.com>
2023-03-05 01:03:20 +0100

    change cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const initializeSocket = require('./socket/socket');
 const app = express();
 // Allow cross-origin requests from http://localhost:4200
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
