b8967f8d635690a71fbe8afe78af0fa9874fc52c
Andrey Zemlyankin <azemlyankin10@gmail.com>
2023-03-06 16:35:38 +0100

    add hello world

--- src/index.js
+++ src/index.js
@@ -9,10 +9,7 @@ const initializeSocket = require('./socket/socket');
 const app = express();
 // Allow cross-origin requests from http://localhost:4200
 app.use((req, res, next) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        'https://angular-nodejs-realtime-frontend.vercel.app'
-    );
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
@@ -27,6 +24,10 @@ app.use(
 app.use(express.json());
 app.use(express.static('public'));
 const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
 
 mongoose
     .connect(process.env.DB_URI, { useNewUrlParser: true })
